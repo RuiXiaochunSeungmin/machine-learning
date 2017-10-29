4f1682335cb775411d3c4b3beb2258df97916685
Xiaochun Ma <122030478@qq.com>
2017-10-29 13:18:19 -0400

    hw3.3
    Accuracy: 0.842.

---
+++ three/initial_trail.py
@@ -0,0 +1,203 @@
+# -*- coding:utf8 -*-
+import codecs
+from collections import defaultdict
+import glob
+import re
+import scipy.io
+import numpy as np
+import pandas as pd
+from sklearn import tree
+
+NUM_TRAINING_EXAMPLES = 4372
+NUM_TEST_EXAMPLES = 800
+
+BASE_DIR = './'
+SPAM_DIR = 'spam/'
+HAM_DIR = 'ham/'
+TEST_DIR = 'test_data/'
+
+#---------Feature extraction----------#
+
+# Features that look for certain words
+def freq_pain_feature(text, freq):  #0
+    return float(freq['pain'])
+
+def freq_private_feature(text, freq):
+    return float(freq['private'])
+
+def freq_bank_feature(text, freq):
+    return float(freq['bank'])
+
+def freq_money_feature(text, freq):
+    return float(freq['money']) #3
+
+def freq_drug_feature(text, freq):
+    return float(freq['drug'])
+
+def freq_spam_feature(text, freq):
+    return float(freq['spam'])
+
+def freq_prescription_feature(text, freq):
+    return float(freq['prescription'])
+
+def freq_creative_feature(text, freq):
+    return float(freq['creative'])
+
+def freq_height_feature(text, freq): #8
+    return float(freq['height'])
+
+def freq_featured_feature(text, freq):
+    return float(freq['featured'])
+
+def freq_differ_feature(text, freq):
+    return float(freq['differ'])
+
+def freq_width_feature(text, freq):
+    return float(freq['width'])
+
+def freq_other_feature(text, freq):
+    return float(freq['other'])
+
+def freq_energy_feature(text, freq): #13
+    return float(freq['energy'])
+
+def freq_business_feature(text, freq):
+    return float(freq['business'])
+
+def freq_message_feature(text, freq):
+    return float(freq['message'])
+
+def freq_volumes_feature(text, freq):
+    return float(freq['volumes'])
+
+def freq_revision_feature(text, freq):
+    return float(freq['revision'])
+
+def freq_path_feature(text, freq): #18
+    return float(freq['path'])
+
+def freq_meter_feature(text, freq):
+    return float(freq['meter'])
+
+def freq_memo_feature(text, freq):
+    return float(freq['memo'])
+
+def freq_planning_feature(text, freq):
+    return float(freq['planning'])
+
+def freq_pleased_feature(text, freq):
+    return float(freq['pleased'])
+
+def freq_record_feature(text, freq): #22
+    return float(freq['record'])
+
+def freq_out_feature(text, freq):
+    return float(freq['out'])
+
+# Features that look for certain characters
+def freq_semicolon_feature(text, freq):
+    return text.count(';')
+
+def freq_dollar_feature(text, freq):
+    return text.count('$')
+
+def freq_sharp_feature(text, freq):
+    return text.count('#')
+
+def freq_exclamation_feature(text, freq): #27
+    return text.count('!')
+
+def freq_para_feature(text, freq):
+    return text.count('(')
+
+def freq_bracket_feature(text, freq):
+    return text.count('[')
+
+def freq_and_feature(text, freq): #30
+    return text.count('&')
+
+# Generates a feature vector
+def generate_feature_vector(text, freq):
+    feature = []
+    feature.append(freq_pain_feature(text, freq))
+    feature.append(freq_private_feature(text, freq))
+    feature.append(freq_bank_feature(text, freq))
+    feature.append(freq_money_feature(text, freq))
+    feature.append(freq_drug_feature(text, freq))
+    feature.append(freq_spam_feature(text, freq))
+    feature.append(freq_prescription_feature(text, freq))
+    feature.append(freq_creative_feature(text, freq))
+    feature.append(freq_height_feature(text, freq))
+    feature.append(freq_featured_feature(text, freq))
+    feature.append(freq_differ_feature(text, freq))
+    feature.append(freq_width_feature(text, freq))
+    feature.append(freq_other_feature(text, freq))
+    feature.append(freq_energy_feature(text, freq))
+    feature.append(freq_business_feature(text, freq))
+    feature.append(freq_message_feature(text, freq))
+    feature.append(freq_volumes_feature(text, freq))
+    feature.append(freq_revision_feature(text, freq))
+    feature.append(freq_path_feature(text, freq))
+    feature.append(freq_meter_feature(text, freq))
+    feature.append(freq_memo_feature(text, freq))
+    feature.append(freq_planning_feature(text, freq))
+    feature.append(freq_pleased_feature(text, freq))
+    feature.append(freq_record_feature(text, freq))
+    feature.append(freq_out_feature(text, freq))
+    feature.append(freq_semicolon_feature(text, freq))
+    feature.append(freq_dollar_feature(text, freq))
+    feature.append(freq_sharp_feature(text, freq))
+    feature.append(freq_exclamation_feature(text, freq))
+    feature.append(freq_para_feature(text, freq))
+    feature.append(freq_bracket_feature(text, freq))
+    feature.append(freq_and_feature(text, freq))
+
+    return feature
+
+# This method generates a design matrix with a list of filenames
+# Each file is a single training example
+def generate_design_matrix(filenames):
+    design_matrix = []
+    for filename in filenames:
+        with open(filename) as f:
+            text = f.read() # Read in text from file
+            text = text.replace('\r\n', ' ') # Remove newline character
+            words = re.findall(r'\w+', text)
+            word_freq = defaultdict(int) # Frequency of all words
+            for word in words:
+                word_freq[word] += 1
+
+            # Create a feature vector
+            feature_vector = generate_feature_vector(text, word_freq)
+            design_matrix.append(feature_vector)
+    return design_matrix
+
+#--------------Execution on files---------------------#
+spam_filenames = glob.glob(BASE_DIR + SPAM_DIR + '*.txt')
+spam_design_matrix = generate_design_matrix(spam_filenames)
+ham_filenames = glob.glob(BASE_DIR + HAM_DIR + '*.txt')
+ham_design_matrix = generate_design_matrix(ham_filenames)
+
+
+test_filenames = [BASE_DIR + TEST_DIR + 'test_email_'+ str(x) + '.txt' for x in range(1,NUM_TEST_EXAMPLES+1)]
+test_design_matrix = generate_design_matrix(test_filenames)
+
+X = spam_design_matrix + ham_design_matrix
+Y = [1]*len(spam_design_matrix) + [0]*len(ham_design_matrix)
+
+file_dict = {}
+file_dict['training_data'] = X
+file_dict['training_labels'] = Y
+file_dict['test_data'] = test_design_matrix
+scipy.io.savemat('spam_data.mat', file_dict)
+
+#--------------Load, fit and Predict------------------#
+mat= scipy.io.loadmat('spam_data.mat')
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(mat['training_data'], np.transpose(mat['training_labels']))
+dt_result = clf.predict(mat['test_data'])
+df_r = pd.DataFrame(dt_result)
+df_r.to_csv('first.csv')
+# After saving csv, we need to rename column names and 
+# Rearrange row numbers. For simplicity, I did it using
+# Excel.
